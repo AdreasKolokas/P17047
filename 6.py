9711e7f352e3d00c8962cadfdcfc78e42fd77c9d
AdreasKolokas <36745133+AdreasKolokas@users.noreply.github.com>
2018-02-22 22:54:23 +0200

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,5 @@
+import calendar
+
+print(calendar.month(int(input("Γράψε το έτος που θες(αριθμό): ")),int(input("Γράψε τον μήνα που θες(αριθμό): "))))
+
+input("Πάτα το ENTER για να τερματίσεις το πρόγραμμα.")
